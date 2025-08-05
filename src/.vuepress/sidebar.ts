0f0d2fa3f080db1e37e4b4f4ba4ae9915e62e74b
yyyytawa <116234899+yyyytawa@users.noreply.github.com>
2025-08-05 20:27:01 +0800

    fix: 修复/api/wss/ 侧边栏

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,10 +1,8 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar(
-  {
-    "/api/v1": "structure",
-  },
-  {
-    "/api/wss": "structure",
-  }
+    {
+    "/api/v1/": "structure",
+    "/api/wss/": "structure",
+    }
 );
