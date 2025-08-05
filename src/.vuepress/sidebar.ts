7e8714d73adb7ce5343b2c8bd004b27caebe18d6
QianLin-Jiaxi <17850560805@163.com>
2025-08-05 11:45:08 +0000

    Update sidebar.ts

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,5 +1,10 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export default sidebar({
-  "/api/v1": "structure",
-});
+export default sidebar(
+  {
+    "/api/v1": "structure",
+  }
+  {
+    "/api/wss": "structure",
+  }
+);
