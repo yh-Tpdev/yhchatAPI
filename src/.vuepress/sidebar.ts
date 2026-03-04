9e4b337c901d8f7e3283546cdc51987798437362
ccd2s <tasaedem@outlook.com>
2026-03-04 23:59:53 +0800

    fix: sidebar missing

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -2,9 +2,13 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar(
     {
-    "/api/v1/": "structure",
-    "/api/wss/": "structure",
-    "/云湖API吐槽/": "structure",
-    "/api/web/v1/": "structure",
+        "/api/": "structure",
+        "/api/v1/": "structure",
+        "/api/wss/": "structure",
+        "/云湖API吐槽/": "structure",
+        "/api/web/v1/": "structure",
+        "/api/bot/": "structure",
+        "/api/web/": "structure",
+        "/api/bot/接入准备/": "structure"
     }
 );
